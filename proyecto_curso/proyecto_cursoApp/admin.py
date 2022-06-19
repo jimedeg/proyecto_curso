dd83e2a5d84ee13226a567f9e0d9a8e5e4a5f0fb
jimedeg <jimena.degiorgi@gmail.com>
2022-06-18 23:21:50 -0300

    creando admin

--- proyecto_curso/proyecto_cursoApp/admin.py
+++ proyecto_curso/proyecto_cursoApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from proyecto_cursoApp.models import Curso, Evento
+
+# Register your models here.
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'info', 'comision')
+
+admin.site.register(Curso, CursoAdmin)
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'info', 'fecha')
+
+admin.site.register(Evento, EventoAdmin)
+
+#user= admin - contraseña= 1234

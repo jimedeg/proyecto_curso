5faff18861baebc9dcc15a71660e9a364c754466
jimedeg <jimena.degiorgi@gmail.com>
2022-06-18 23:00:21 -0300

    color barra de navegacion

--- proyecto_curso/proyecto_cursoApp/views.py
+++ proyecto_curso/proyecto_cursoApp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def inicio (request):
     
-    return render(request,"proyecto_cursoApp/base.html",{})
+    return render(request,"proyecto_cursoApp/index.html",{})
 
 def curso (request):
     

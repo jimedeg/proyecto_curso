b80bb946fd625c61825fb6ad0c04e919eab40102
jimedeg <jimena.degiorgi@gmail.com>
2022-07-15 00:32:43 -0300

    url lista de inicio

--- proyecto_curso/proyecto_curso/urls.py
+++ proyecto_curso/proyecto_curso/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('proyecto_cursoApp/', include('proyecto_cursoApp.urls')),
+    path('', include('proyecto_cursoApp.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
